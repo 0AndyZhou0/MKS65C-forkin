1acdb86e9a30e8c911159ba8838a9c0484a57b4e
0AndyZhou0 <azhou4@stuy.edu>
2018-11-18 17:20:27 -0500

    forks are done I think

--- forkin.c
+++ forkin.c
@@ -4,18 +4,25 @@
 #include <unistd.h>
 
 int main(){
-	printf("Hello There\n");
-	pid_t child1, child2;
-	child1 = fork();
-	int status;
-	child1 = wait(&status);
-	if(child1 != getpid()){
-		child2 = fork();
-	}else{
-		printf("parent pid : %i\n",getppid());
-		printf("pid : %i\n",getpid());
-	}
-	//printf("%d\n",status);
-	printf("done\n");
-	return 0;
+  printf("Hello There\n");
+  pid_t child1, child2;
+  child1 = fork();
+  if(child1){
+    child2 = fork();
+    if(child2){
+      printf("I'm the parent\n");
+      printf("parent pid : %i\n",getppid());
+      printf("pid : %i\n",getpid());
+    }else{
+      printf("I'm child2\n");
+      printf("parent pid : %i\n",getppid());
+      printf("pid : %i\n",getpid());
+    }
+  }else{
+    printf("I'm child1\n");
+    printf("parent pid : %i\n",getppid());
+    printf("pid : %i\n",getpid());
+  }
+  printf("done\n");
+  return 0;
 }
